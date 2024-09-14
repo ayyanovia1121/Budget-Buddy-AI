27e2d4f90200287dfe234ba39e4d69a90de10f03
ayyanovia1121 <noviayya1121@gmail.com>
2024-09-14 18:17:36 +0700

    Header Component

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -1,21 +1,33 @@
 'use client'
 
+import { Button } from "@/components/ui/button";
+import { UserButton, useUser } from "@clerk/nextjs";
+import Link from "next/link";
 import React from 'react'
 
 const Header = () => {
-  // const {user, isSignedIn} = useUser();
+  const {user, isSignedIn} = useUser();
   return (
-    <div className='p-5 flex justify-between items-center border shadow-sm'>
-      <div className='flex flex-row items-center'>
+    <div className="p-5 flex justify-between items-center border shadow-sm">
+      <div className="flex flex-row items-center">
         {/* <Image src='/logo.png' width={40} height={25} alt='logo' /> */}
-        <span>BudgetBuddy.ai</span>
+        <span className="text-primary font-bold text-xl">BudgetBuddy.ai</span>
       </div>
-      <div>
-        <button>Button 1</button>
-        <button>Button 2</button>
-      </div>
+
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <div className="flex gap-3 items-center">
+          <Link href="/dashboard">
+            <Button variant="outline" className="rounded-full">Dashboard</Button>
+          </Link>
+          <Link href="/dashboard">
+            <Button variant="default" className="rounded-full">Get Started</Button>
+          </Link>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default Header
